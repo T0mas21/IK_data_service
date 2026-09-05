e41f9aad70db40f731a794face142ba1db492167
Tomáš Janečka <tomjanecka11@gmail.com>
2026-09-05 17:31:20 +0200

    swagger ui file upload

--- ConfigService/src/main/java/org/config/rest/ConfigApi.java
+++ ConfigService/src/main/java/org/config/rest/ConfigApi.java
@@ -8,6 +8,7 @@ import org.config.dto.FileDto;
 import org.config.facade.ConfigFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -60,7 +61,7 @@ public class ConfigApi {
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/{configId}/files")
+    @PostMapping(value = "/{configId}/files", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<FileDto> uploadFile(@PathVariable Long configId,
                                                @RequestParam("file") MultipartFile file) {
         FileDto uploaded = configFacade.uploadFile(configId, file);
